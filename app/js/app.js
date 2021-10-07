1553c05df8345851a78892b2e47e7cb5e8e725b3
livefortoday89 <livefortoday@mail.ru>
2021-10-07 19:21:35 +0300

    Oct 7

---
+++ app/js/app.js
@@ -0,0 +1,9 @@
+// import $ from 'jquery'; window.jQuery = $; window.$ = $ // import jQuery module (npm i -D jquery)
+
+// require('~/app/libs/mmenu/js/jquery.mmenu.all.min.js') // import vendor jQuery plugin example (not module)
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Custom JS
+
+})
